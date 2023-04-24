111d4647ec0d37de896c5f3a7ce178f834d36fc7
Rapha-rsa-dev <raphael.123.rsa@gmail.com>
2023-04-24 00:28:57 -0300

    inicialização de formulario

--- SRC/assets/js/app.js
+++ SRC/assets/js/app.js
@@ -1,4 +1,17 @@
+const addNewPaciente = document.querySelector('.newPacientAdd');
+const modalPaciente = document.querySelector('.modal-paciente');
+const fecharModalPaciente = document.querySelector('#closeModal');
 
+
+addNewPaciente.addEventListener('click', addPaciente);
+fecharModalPaciente.addEventListener('click', closeModal);
+
+function addPaciente(){
+    modalPaciente.style.display = 'flex';
+};
+function closeModal(){
+    modalPaciente.style.display = 'none';
+};
 
 
 
