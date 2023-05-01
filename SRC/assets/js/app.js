72444486964615f0f4a0e1b5cf8b41281647cbaf
Rapha-rsa-dev <raphael.123.rsa@gmail.com>
2023-05-01 19:38:19 -0300

    paginando o projeto

--- SRC/assets/js/app.js
+++ SRC/assets/js/app.js
@@ -37,23 +37,37 @@ const modDelete = document.querySelector('.modDelete');
 const btnNoDelete = document.querySelector('.no');
 const btnDelete = document.querySelector('.yes');
 const emailCheck = document.getElementsByClassName('emailInput');
-const sairPopup = document.querySelector('#arrow-down')
+const sairPopup = document.querySelector('#arrow-down');
 const modalSair = document.querySelector('#popup');
-const sair = document.querySelector("#sair")
-
+const sair = document.querySelector("#sair");
+const body = document.querySelector('.main-conteiner');
+const cabecalho = document.querySelector('.contentHeader');
+const centro = document.querySelector('.contentTable');
 
 sairPopup.addEventListener("click", e => {
     modalSair.style.display = 'flex';
 })
-modalSair.addEventListener('click', function (event) {
-
-    if (event.target === modalSair) {
+centro.addEventListener('click', function (event) {
+    if (event.target === centro) {
         modalSair.style.display = 'none';
     }
 });
-sairPopup.addEventListener("click", e => {
-    modalSair.style.display = 'flex';
-})
+
+body.addEventListener('click', function (event) {
+    if (event.target === body) {
+        modalSair.style.display = 'none';
+    }
+});
+cabecalho.addEventListener('click', function (event) {
+    if (event.target === cabecalho) {
+        modalSair.style.display = 'none';
+    }
+});
+document.addEventListener('click', function (event) {
+    if (modalSair.contains(event.target)) {
+        modalSair.style.display = 'none';
+    }
+});
 
 sair.addEventListener("click", e => {
     modalSair.style.display = 'none';
@@ -70,7 +84,7 @@ const nomeU = fetch(`https://raphadev.onrender.com/login`)
         response.json()
             .then((dados) => {
                 dados.map((login) => {
-                    console.log(login)
+                   
                     if (login) {
                         document.querySelector('#userName').textContent = `${login.nomeUser}`
                         document.querySelector('.emailLogado').textContent = `${login.emailUser}`
@@ -89,6 +103,9 @@ function addPaciente() {
     document.querySelector('.contentTable').style.display = 'none';
     document.querySelector('#back').style.backgroundColor = 'rgba(0,0,0,0.4)';
 };
+function closeModal(){
+    modConfirm.style.display = "block";
+}    
 closeCancel.addEventListener('click', function () {
     modConfirm.style.display = "block";
 });
@@ -131,7 +148,7 @@ fetch(`https://raphadev.onrender.com/login`).then((response) => {
 
             if (login) {
                 nomeUser = `${login.nomeUser}`
-                console.log(nomeUser)
+                
             }
 
         })
@@ -366,50 +383,154 @@ enviarForm.addEventListener('DOMContentLoaded', function () {
     }, 2000);
     return;
 });
-function viwerPaciente() {
 
-    //ainda vou fazer
-};
-function editarPaciente() {
-    modalPaciente.style.display = 'flex';
-    document.querySelector('.paceiteTitulotext').innerText = 'Dados do Paciente';
-    document.querySelector('.send').style.display = 'none';
-    document.querySelector('#editar').style.display = 'block';
     const labels = document.querySelectorAll('label');
     const spans = document.querySelectorAll('span');
     const inputs = document.querySelectorAll('input');
     const selects = document.querySelectorAll('select');
-        
- 
-    for (let i = 0; i < labels.length; i++) {
-        labels[i].remove();
-    }
-    for (let i = 0; i < spans.length; i++) {
-        spans[i].remove();
-    }
-    inputs.forEach(input => {
-        input.disabled = true;
-      });
-      inputs.forEach(input => {
-        input.style.backgroundColor = '#BDBDBD' ;
-      });
-      selects.forEach(select => {
-        select.disabled = true;
-      });
-      selects.forEach(select => {
-        select.style.backgroundColor = '#BDBDBD' ;
-      });
-          
-};
-const btnEditar = document.querySelector('#editar');
-btnEditar.addEventListener('click', function (e) {
-    modalPaciente.style.display = 'flex';
-    document.querySelector('.paceiteTitulotext').innerText = 'Editar dados do Paciente';
-    document.querySelector('.send').style.display = 'flex';
-    btnSend.innerText = 'Salvar alterações';
-    document.querySelector('#editar').style.display = 'flex';
     
-});
+    function viwerPaciente() {
+    document.querySelector('.paceiteTitulotext').innerText = 'Dados do Paciente';
+    document.querySelector('.send').style.display = 'none';
+    document.querySelector('#editar').style.display = 'block';
+    modalPaciente.style.display = 'flex';
+    inputs.forEach(inputs => {
+        inputs.disabled = true;
+        inputs.style.backgroundColor = '#BDBDBD';
+    });
+    selects.forEach(selects => {
+        selects.disabled = true;
+        selects.style.backgroundColor = '#BDBDBD';
+    });
+    labels.forEach(labels => {
+        labels.style.display = 'none';
+    });
+    spans.forEach(spans => {
+        spans.style.display = 'none';
+    });
+       
+    function dadosPaciente(paciente) {
+        cpfInput.value = paciente.cpf;
+        nameInputPaciente.value = paciente.nome;
+        dataDeNascInput.value = paciente.dataDeNascimento;
+        emailInputPaciente.value = paciente.email;
+        generoInput.value = paciente.genero;
+        nacionalidadeInput.value = paciente.nacionalidade;
+        naturalidadeInput.value = paciente.naturalidade;
+        profissaoInput.value = paciente.profissao;
+        escolaridadeInput.value = paciente.escolaridade;
+        estadoCivilInput.value = paciente.estadoCivil;
+        maeInput.value = paciente.mae;
+        paiInput.value = paciente.pai;
+    };
+    async function carregarDadosPaciente() {
+        try {
+          const response = await fetch(`https://raphadev.onrender.com/pacientes/1`);
+          const data = await response.json();
+          
+          dadosPaciente(data);
+        } catch (error) {
+          console.error(error);
+          alert('Não foi possível carregar os dados do paciente');
+        }
+      }
+      
+      carregarDadosPaciente(); 
+      
+}    
+    
+    
+    btnEditar = document.querySelector('#editar');
+    btnEditar.addEventListener('click', function (e) {
+        modalPaciente.style.display = 'flex';
+        document.querySelector('.paceiteTitulotext').innerText = 'Editar dados do Paciente';
+        document.querySelector('.send').style.display = 'flex';
+        document.querySelector('#sendPacient').style.display ='none';
+        document.querySelector('#editar').style.display = 'none';
+        const divEdit = document.querySelector('.btnsend');
+        
+        divEdit.innerHTML = '<p onclick="closeModal()" class="cancel">Cancelar</p><button onclick="editarPac()" type="submit" id="saveEdit">Salvar alterações</button>' 
+          
+        inputs.forEach(inputs => {
+            inputs.disabled = false;
+            inputs.style.backgroundColor = '#FFFFFF';
+        });
+        selects.forEach(selects => {
+            selects.disabled = false;
+            selects.style.backgroundColor = '#FFFFFF';
+        });
+        labels.forEach(labels => {
+            labels.style.display = 'flex';
+        });
+        spans.forEach(spans => {
+            spans.style.display = 'flex';
+        });
+                
+             
+        })
+        const dados = {
+            cpf: cpfInput.value,
+            nome: nameInputPaciente.value,
+            dataDeNascimento: dataDeNascInput.value,
+            email: emailInputPaciente.value,
+            genero: generoInput.value,
+            nacionalidade: nacionalidadeInput.value,
+            naturalidade: naturalidadeInput.value,
+            profissao: profissaoInput.value,
+            escolaridade: escolaridadeInput.value,
+            estadoCivil: estadoCivilInput.value,
+            mae: maeInput.value,
+            pai: paiInput.value
+          }
+
+        async function editarPac() {
+            
+               try {
+                const dados = {
+                  cpf: cpfInput.value,
+                  nome: nameInputPaciente.value,
+                  dataDeNascimento: dataDeNascInput.value,
+                  email: emailInputPaciente.value,
+                  genero: generoInput.value,
+                  nacionalidade: nacionalidadeInput.value,
+                  naturalidade: naturalidadeInput.value,
+                  profissao: profissaoInput.value,
+                  escolaridade: escolaridadeInput.value,
+                  estadoCivil: estadoCivilInput.value,
+                  mae: maeInput.value,
+                  pai: paiInput.value
+                };
+                
+                const response = await fetch(`https://raphadev.onrender.com/pacientes/1`, {
+                  method: 'PUT',
+                  headers: {
+                    'Content-Type': 'application/json'
+                  },
+                  body: JSON.stringify(dados)
+                })
+                
+            
+                if (!response.ok) {
+                  throw new Error('Não foi possível atualizar os dados do paciente');
+                }
+                
+                const data = await response.json();
+                closeModal();
+                console.log(data);
+                alert('Dados do paciente atualizados com sucesso');
+                document.location.reload()
+              } catch (error) {
+                console.error(error);
+                alert(error.message);
+              }
+            
+
+        }
+      
+
+
+
+
 function deletarPacient() {
     modDelete.style.display = 'block';
     document.querySelector('#text6').innerText = 'Essa ação é irreversivel';
@@ -431,8 +552,8 @@ function exibirPacientes(pacientes) {
         <td id="nomeUser1">${paciente.nome}</td>
         <td id="cpfUser">${paciente.cpf}</td>
         <td id="iconn">
-        <i onclick="viwerPaciente()" class="fa-sharp fa-solid fa-clipboard-list" id="clipboard"></i>
-        <i onclick="editarPaciente()" class="fa-sharp fa-solid fa-pen-line" id="penLine"><img
+        <i  class="fa-sharp fa-solid fa-clipboard-list allclip" id="clipboard"></i>
+        <i onclick="viwerPaciente()" class="fa-sharp fa-solid fa-pen-line umpen" id="penLine"><img
                 src="./SRC/assets/img/pen_line.svg" alt=""></i>
         <i onclick="deletarPacient()" class="fa-solid fa-trash-can" id="trash"></i>
         </td>
@@ -453,7 +574,7 @@ fetch('https://raphadev.onrender.com/pacientes')
                 const idMedico = medicoLogado.id;
                 const medico = medicosData.find(medico => medico.id === idMedico);
                 const pacientes = data.filter(paciente => paciente.idMedico && paciente.idMedico === medico.id);
-                console.log(`Pacientes do médico ${medico.idMedico}:`, pacientes);
+                // console.log(`Pacientes do médico ${medico.idMedico}:`, pacientes);
             });
     });
 function exibirPaginacao(pacientes) {
@@ -489,6 +610,7 @@ async function carregarDados() {
 };
 
 
+
 async function deletarDados(id) {
     await fetch(apiURL + `/${id}`, {
         method: 'DELETE'
@@ -505,3 +627,58 @@ btnDelete.addEventListener('click', async () => {
     }, 1500);
 });
 carregarDados();
+// to aqui
+const searchInput = document.querySelector('.search');
+const pacientesDiv = document.querySelector('.pacientes');
+let timeoutId = null;
+let acharPaciente = '';
+
+function renderizarPacientes(pacientes) {
+    pacientesDiv.innerHTML = '';
+    pacientes.forEach(paciente => {
+        if (paciente.nome.toLowerCase().startsWith(acharPaciente)) {
+            const divPaciente = document.createElement('div');
+            divPaciente.classList.add('paciente');
+            const nomeElement = document.createElement('p');
+            nomeElement.textContent = paciente.nome;
+            divPaciente.appendChild(nomeElement);
+            pacientesDiv.appendChild(divPaciente);
+            pacientesDiv.style.display = 'block';
+
+            //     divPaciente.addEventListener('click', () => {
+            //criar caminho para pagina obs anda nao criei
+            //         window.location.href = `https://seudominio.com/prontuario/${paciente.id}`;
+            //       });
+        }
+    });
+}
+function handleSearchInput(event) {
+    const searchTerm = event.target.value.trim().toLowerCase();
+    const firstWord = searchTerm.split(' ')[0];
+    if (timeoutId) {
+        clearTimeout(timeoutId);
+    }
+    timeoutId = setTimeout(() => {
+        if (firstWord === acharPaciente) {
+            return;
+        }
+        acharPaciente = firstWord;
+
+        const endpoint = `https://raphadev.onrender.com/pacientes?search=${searchTerm}`;
+        fetch(endpoint)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Erro ao buscar pacientes!');
+                }
+                return response.json();
+            })
+            .then(data => {
+                renderizarPacientes(data);
+            })
+            .catch(error => {
+                console.error(error);
+                pacientesDiv.innerHTML = '<p>Erro ao buscar pacientes!</p>';
+            });
+    }, 500);
+}
+searchInput.addEventListener('input', handleSearchInput);
